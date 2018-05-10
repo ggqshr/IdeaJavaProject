f8e97b5661e7760e8877717dc005d5e569854d14
ggq <ggq18663278150@sina.com>
2018-05-10 20:12:06 +0800

    根据情感进行partition和对数据进行清洗

---
+++ hadoopProject/src/Hive/UDFCase/WishDataUDF.java
@@ -0,0 +1,11 @@
+package Hive.UDFCase;
+
+import org.apache.hadoop.hive.ql.exec.UDF;
+
+public class WishDataUDF extends UDF {
+    public String evaluate(String line) {
+        String line1 = line.replaceAll("\n", "");
+        String line2 = line1.replaceAll("\r", "");
+        return line2;
+    }
+}
