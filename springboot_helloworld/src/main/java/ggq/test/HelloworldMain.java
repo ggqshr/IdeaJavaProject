94611d31813a24dc59c57d97276e88315abdd9f4
ggqshr <942490944@qq.com>
2018-08-28 15:09:38 +0800

    springboot hellowrold

---
+++ springboot_helloworld/src/main/java/ggq/test/HelloworldMain.java
@@ -0,0 +1,11 @@
+package ggq.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloworldMain {
+    public static void main(String[] args) {
+        SpringApplication.run(HelloworldMain.class, args);
+    }
+}
